5329af2d95271d854cc13a1c09f65586d37f89e1
unknown <sarunas.s.1993@gmail.com>
2018-03-15 16:42:55 +0200

    keli uzdavyniai

--- src/lt/kcs/utils/MyMainCalculator2.java
+++ src/lt/kcs/utils/MyMainCalculator2.java
@@ -6,6 +6,29 @@ public class MyMainCalculator2 {
 
         Skaiciavimai2.printRectangle(9,4);
 
+        Skaiciavimai2.dvimatis();
+
+//        int[][] lentele = new int[10][10];
+//
+//        for (int i = 1; i < lentele.length; i++) {
+//            for (int j = 1; j < lentele[i].length; j++) {
+//                lentele[i][j] = i * j;
+//            }
+//        }
+//
+//        for (int i = 1; i < lentele.length; i++) {
+//            for (int j = 1; j < lentele[i].length; j++) {
+//                System.out.print(lentele[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        Skaiciavimai2.fibonacci(10);
+
+        System.out.println();
+
+        Skaiciavimai2.faktiorialas(4);
+
 
     }
 }
--- src/lt/kcs/utils/Skaiciavimai2.java
+++ src/lt/kcs/utils/Skaiciavimai2.java
@@ -13,4 +13,43 @@ public class Skaiciavimai2 {
         }
 
     }
+
+    public static void dvimatis() {
+
+        int[][] dvimatisMasyvas = new int[10][5];
+
+        for (int a = 0; a < dvimatisMasyvas.length; a++) {
+            for (int b = 0; b < dvimatisMasyvas[a].length; b++) {
+                System.out.print(dvimatisMasyvas[a][b]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void fibonacci(int kartai) {
+        int fibonaciukas1 = 0;
+        int fibonaciukas2 = 1;
+        int fibonaciukas3;
+
+        int i;
+
+        System.out.print(fibonaciukas1 + " " + fibonaciukas2);
+
+        for (i = 2; i < kartai; i++) {
+            fibonaciukas3 = fibonaciukas1 + fibonaciukas2;
+            System.out.print(" " + fibonaciukas3);
+            fibonaciukas1 = fibonaciukas2;
+            fibonaciukas2 = fibonaciukas3;
+        }
+
+    }
+    public static void faktiorialas(int ikiKiek) {
+        int sum = 1;
+        for (int i = 1; i <= ikiKiek; i++) {
+            sum = sum * i;
+        }
+        System.out.println(sum);
+    }
+
 }
